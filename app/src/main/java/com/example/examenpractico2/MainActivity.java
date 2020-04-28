35f6e01a714d06c1173438e57bdb4ba5a65c5bf6
IsaacoAC <57645974+IsaacoAC@users.noreply.github.com>
2020-04-28 16:28:47 -0600

    Layout de la lista, Layout personalizado

--- app/src/main/java/com/example/examenpractico2/MainActivity.java
+++ app/src/main/java/com/example/examenpractico2/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.examenpractico2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intCapt, intMostrar;
+    Button btnCapt,btnMostrar,btnSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCapt=findViewById(R.id.btnCapturar);
+        btnMostrar=findViewById(R.id.btnMostrar);
+        btnSalir=findViewById(R.id.btnSalir);
+    }
+
+    public void onClickCapturar(View v){
+        intCapt = new Intent(this,CapturarNuevo.class);
+        startActivity(intCapt);
+    }
+
+    public void onClickSalir(View v){
+        onDestroy();
+    }
+
+    public void inClickMostrar(View v){
+        intMostrar= new Intent(this, ListaDeRestaurantes.class);
+        startActivity(intMostrar);
     }
 }
