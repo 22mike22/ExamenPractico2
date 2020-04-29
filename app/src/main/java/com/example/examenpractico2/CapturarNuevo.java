051c2456ca3ec243854d52a91c6a49e0e6ea40b5
22mike22 <miguel_222220@hotmail.com>
2020-04-28 18:59:55 -0600

    a

--- app/src/main/java/com/example/examenpractico2/CapturarNuevo.java
+++ app/src/main/java/com/example/examenpractico2/CapturarNuevo.java
@@ -3,12 +3,25 @@ package com.example.examenpractico2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class CapturarNuevo extends AppCompatActivity {
+public class CapturarNuevo extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capturar_nuevo);
+        ImageView imgSelecionada=findViewById(R.id.imgSelect);
+        imgSelecionada.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgSelect:
+                //lansar actividad para elegir imagen;
+                break;
+        }
     }
 }
